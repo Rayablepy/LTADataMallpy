833b2e9dbe44948b179948b478d161f86288b60a
Rayablepy <rayhan11.0808@gmail.com>
2026-08-25 22:01:52 +0800

    fixed bugs in inheritance setup

--- BusArrival.py
+++ BusArrival.py
@@ -3,15 +3,18 @@ load_dotenv()
 from main import base_url
 import httpx
 import os
-
+base_url="https://datamall2.mytransport.sg/ltaodataservice/"
 key=os.getenv("LTADATAMALL_API_KEY")
 class Bus:
-    def __init__(self,url=None,api_key:str):
+    def __init__(self,api_key:str,url=None):
         self.url=url
         self.api_key=api_key
         self.headers={"AccountKey":api_key}
 
 class BusArrival(Bus):
+    def __init__(self,api_key:str):
+        super().__init__(api_key=api_key)
+        self.url=base_url+"v3/BusArrival"
     def get_bus_arrival(self,stopcode:str,serviceno=None):
         if not serviceno:
             params={
@@ -26,6 +29,6 @@ class BusArrival(Bus):
             }
             r=httpx.get(self.url,headers=self.headers,params=params).json()
             return r
-arrivals = BusArrival(key)
+main=BusArrival(api_key=key)
 
-print(arrivals.get_bus_arrival("83139"))
+print(main.get_bus_arrival("83139"))
