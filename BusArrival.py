90c602c0e0f65ed82aaa17d0e5195a94c0d9c7a2
Rayablepy <rayhan11.0808@gmail.com>
2026-08-25 22:08:56 +0800

    Rebuilt inheritance logic and serviceno checks

--- BusArrival.py
+++ BusArrival.py
@@ -3,32 +3,26 @@ load_dotenv()
 from main import base_url
 import httpx
 import os
-base_url="https://datamall2.mytransport.sg/ltaodataservice/"
 key=os.getenv("LTADATAMALL_API_KEY")
 class Bus:
-    def __init__(self,api_key:str,url=None):
-        self.url=url
+    def __init__(self,api_key:str):
         self.api_key=api_key
         self.headers={"AccountKey":api_key}
+        self.arrival=BusArrival(api_key)
 
-class BusArrival(Bus):
+class BusArrival:
     def __init__(self,api_key:str):
-        super().__init__(api_key=api_key)
         self.url=base_url+"v3/BusArrival"
+        self.headers={"AccountKey":api_key}
     def get_bus_arrival(self,stopcode:str,serviceno=None):
-        if not serviceno:
-            params={
-                "BusStopCode":stopcode
-            }
-            r=httpx.get(self.url, headers=self.headers, params=params).json()
-            return r
-        else:
-            params={
-                "BusStopCode":stopcode,
-                "ServiceNo":serviceno
-            }
-            r=httpx.get(self.url,headers=self.headers,params=params).json()
-            return r
-main=BusArrival(api_key=key)
+        params={
+            "BusStopCode":stopcode
+        }
+        if serviceno:
+            params["ServiceNo"]=serviceno
+        r=httpx.get(self.url,headers=self.headers,params=params).json()
+        return r
 
-print(main.get_bus_arrival("83139"))
+bus=Bus(api_key=key)
+
+print(bus.arrival.get_bus_arrival("83139"))
