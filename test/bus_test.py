f244405b901bcacbeb3da488053fc30fdc51e52d
Rayablepy <rayhan11.0808@gmail.com>
2026-08-28 12:27:53 +0800

    Updated test files

--- test/bus_test.py
+++ test/bus_test.py
@@ -1,11 +1,3 @@
-import sys
-from pathlib import Path
-sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 from dotenv import load_dotenv
-import os
 load_dotenv()
-from main import DataMall
-
-Datamall=DataMall(api_key=os.getenv("LTADATAMALL_API_KEY"))
-
-print(Datamall.bus.routes.get_bus_routes())
+from .main import D
